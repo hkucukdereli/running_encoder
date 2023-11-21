d2668d10183ffafe0500e2d0d68cf1211c0d7ba5
Hakan Kucukdereli <hkucukdereli@gmail.com>
2023-11-21 12:41:03 -0500

    arduino script that runs on the encoder board.

---
+++ encoder_sync/encoder_sync.ino
@@ -0,0 +1,66 @@
+#include <Encoder.h>
+
+#define CAM_PIN 4
+
+unsigned long previousMillis = 0; 
+const int pulseDuration = 5;  // ms
+unsigned long pulseStartTime;
+
+bool isPulsing = false;
+bool shouldPulse = false;
+
+unsigned int freq = 50;  // Hz
+unsigned long interval = (1000 / freq); // ms
+
+Encoder myEncoder(2, 3);
+int count=101;
+
+void setup() {
+  pinMode(CAM_PIN, OUTPUT);
+  digitalWrite(CAM_PIN, LOW);
+
+  Serial.begin(115200);  // Start serial communication at 9600 baud rate
+  
+  myEncoder.write(0);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+
+  if (Serial.available() > 0) {
+    char receivedChar = Serial.read();
+    // Check if the received character is 'S'
+    if (receivedChar == 'S') {
+      shouldPulse = true;
+    }
+    else if (receivedChar == 'E') {
+      shouldPulse = false;
+    }
+    else if (receivedChar == 'A') {
+      freq = Serial.parseInt();  // Parse the following integer
+      interval = 1000 / freq;  // Update the interval
+    }
+  }
+
+    if (isPulsing && currentMillis - pulseStartTime >= pulseDuration) {
+      digitalWrite(CAM_PIN, LOW);
+      isPulsing = false;
+    }
+
+
+  if (shouldPulse && currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis;
+      pulseStartTime = currentMillis;
+      isPulsing = true;
+
+      digitalWrite(CAM_PIN, HIGH);
+      count=count+1;
+      long pos;
+      byte *b,m;
+
+      b = (byte *) &pos;  
+      pos = myEncoder.read();
+      Serial.println(pos+count);
+      }
+
+}
